e185edff8a7796dedb159fde3d79d57c23e3bb92
Anatan <zyabkinantonv@gmail.com>
2023-02-17 13:41:31 +0300

    install pods

---
+++ PhotoSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoSearchApp
+//
+//  Created by Anton Zyabkin on 17.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
